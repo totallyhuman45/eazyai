f92add628b67ad91238f00ddb1a1c5396a0b9d3f
totallyhuman45 <renokid33@gmail.com>
2025-04-29 00:41:11 -0700

    Add files via upload

--- src/frame.rs
+++ src/frame.rs
@@ -16,8 +16,17 @@ static CSS: Asset = asset!("/assets/frame.css");
 #[component]
 pub fn Frame() -> Element {
     // cant use signals in this because of stupid signal rules
-    let mut open = use_signal(|| 0);
+    let mut open = use_signal(|| 2);
+    let nav = navigator();              // ← programmatic navigator
     println!("{:?}",open);
+    match open(){
+        0 => nav.push(Route::Data {}),
+        1 => nav.push(Route::Architecture {}),
+        2 => nav.push(Route::Settings {}),
+        3 => nav.push(Route::Run {}),
+        4 => nav.push(Route::Results {}),
+        _ => panic!("not a page how the fuck u get here")
+    };
     rsx! {
 
         document::Stylesheet { href: CSS }
